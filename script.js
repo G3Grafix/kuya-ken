fcfb2794f786ea3982d4ff5c1af30a9eded4fdeb
Nonoy <dhanjosef.37@gmail.com>
2026-06-26 02:15:11 +0800

    disabled the web access

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ window.addEventListener("scroll", () => {
         nav.style.background = "transparent";
     }
 });
+
+const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent);
+
+if (!isMobile) {
+    document.body.innerHTML = `
+        <div style="
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+            font-family:sans-serif;
+            text-align:center;
+        ">
+            <div>
+                <h1 style="color: white;">📱 Mobile Only</h1>
+                <p style="color: white;">This website is only available on mobile devices.</p>
+            </div>
+        </div>
+    `;
+}
